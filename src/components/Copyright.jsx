41734e8e19568c819c46f937ea5395260b698831
Abinithya <abinithya.samdas@gmail.com>
2024-02-06 11:43:26 +0530

    Navbar responsive changes

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,12 +1,12 @@
-import { Typography } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Typography } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Copyright = (props) => {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link style={{textDecoration: 'none', color: 'white'}} color="inherit" to={"/"}>Employee Site</Link>{' '}
+      <Link style={{textDecoration: 'none', color: 'white'}} color="inherit" to={"https://www.linkedin.com/in/abinithyasa/"}>Abinithya</Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
--- src/components/EmpNavbar.jsx
+++ src/components/EmpNavbar.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Person4 from '@mui/icons-material/Person4';
 import { IconButton } from '@mui/material';
 
 const EmpNavbar = () => {
   return (
-  
-        <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-        <IconButton
-            color="inherit"
-            edge="start"
-            sx={{ mr: 2 }}
-          >
-            <Person4 />
-        </IconButton>
-        <Navbar.Brand as={Link} to="/employee-app/home">Employee Site</Navbar.Brand>
-        <Nav className="me-auto">
-            <Nav.Link to="/employee-app/home" as={Link}>Home</Nav.Link>
-            <Nav.Link to="/employee-app/dashboard" as={Link}>Dashboard</Nav.Link>
-            <Nav.Link to="/employee-app/addemployee" as={Link}>Add Employee</Nav.Link>
-        </Nav>
-        </Container>
-      </Navbar>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+        <div className="container-fluid">
+          <IconButton
+              color="inherit"
+              edge="start"
+              sx={{ ml:2, mr: 2 }}
+            >
+              <Person4 />
+          </IconButton>
+          <Link className="navbar-brand" to="/employee-app/home">Employee Site</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/employee-app/home">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/employee-app/dashboard">Dashboard</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/employee-app/addemployee">Add Employee</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
   )
 }
 
